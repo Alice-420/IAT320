1273d66bd99d847d74308fb40b12f85d939ee3b2
Alice-420 <72856937+Alice-420@users.noreply.github.com>
2021-10-27 22:49:28 -0700

    Add files via upload

---
+++ senseoftouch.ino
@@ -0,0 +1,50 @@
+#include <CapacitiveSensor.h>
+
+ 
+#define speaker 11
+
+
+// Set the Send Pin & Receive Pin.
+CapacitiveSensor   cs_2_3 = CapacitiveSensor(2,3);        
+CapacitiveSensor   cs_2_4 = CapacitiveSensor(2,4);         
+CapacitiveSensor   cs_2_5 = CapacitiveSensor(2,5);     
+CapacitiveSensor   cs_2_6 = CapacitiveSensor(2,6);     
+CapacitiveSensor   cs_2_7 = CapacitiveSensor(2,7);       
+
+
+void setup() {
+  cs_2_6.set_CS_AutocaL_Millis(0xFFFFFFFF);     // turn off autocalibrate on channel 1 - just as an example
+  Serial.begin(9600);
+}
+
+void loop() {
+  long start = millis();
+  
+  long soundDo =  cs_2_3.capacitiveSensor(3000);
+  long soundRe =  cs_2_4.capacitiveSensor(3000);
+  long soundMi =  cs_2_5.capacitiveSensor(3000);
+  long soundFa =  cs_2_6.capacitiveSensor(3000);
+  long soundSol =  cs_2_7.capacitiveSensor(3000);
+  
+  if (soundDo > 500) {
+    tone(speaker,131);
+  }
+  if (soundRe > 500) {
+    tone(speaker,100);   
+  }
+  if (soundMi > 500) {
+    tone(speaker,200);
+  }
+  if (soundFa > 500) {
+    tone(speaker,300);
+  }
+  if (soundSol > 500) {
+    tone(speaker,400);
+
+  
+  if (soundDo<=500  &  soundRe<=500  &  soundMi<=500 & soundFa<=500  &  soundSol<=500){
+    noTone(speaker);
+  }
+  
+  delay(10);                             
+}
